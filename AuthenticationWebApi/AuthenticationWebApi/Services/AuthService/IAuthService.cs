b0eee9c5f187948878ab4b3d73d24e9a6d9a41ae
Sertac10 <102825056+Sertac10@users.noreply.github.com>
2022-06-14 19:08:26 +0300

    Add files via upload

---
+++ AuthenticationWebApi/AuthenticationWebApi/Services/AuthService/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticationWebApi.Services.AuthService
+{
+    public interface IAuthService
+    {
+        Task<User> RegisterUser(UserDto request);
+        Task<AuthResponseDto> Login(UserDto request);
+        Task<AuthResponseDto> RefreshToken();
+    }
+}
